94bc3b334958650d0ee501c4e243a077a01cf715
Dina Polousova <dinapolousova@gmail.com>
2023-08-19 23:51:29 +0300

    variables

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
+
+	let calcDate = document.getElementById('date'),
+		couponDate = document.getElementById('coupon-date'),
+		couponAmount = document.getElementById('coupon-amount');
+})
