cf0d29c2f59e00c85457cefe50594b20f5355d33
Dina Polousova <dinapolousova@gmail.com>
2023-08-23 17:44:03 +0300

    date Parse

--- scripts.js
+++ scripts.js
@@ -2,16 +2,26 @@
 window.addEventListener('DOMContentLoaded', function() {
 	'use strict';
 
-	let calcDate = document.getElementById('.date'),
+	let calcDate = document.getElementById('date'),
+
 		couponDate = document.querySelectorAll('.coupon-date'),
 		couponAmount = document.querySelectorAll('.coupon-amount'),
 
 		quantity = document.querySelectorAll('.quantity'),
 		faceValue = document.querySelectorAll('.face-value'),
 		couponPercentage = document.querySelectorAll('.coupon'),
-		couponPeriod = document.querySelectorAll('.coupon-period'),
-		maturityDate = documnet.querySelectorAll('.maturity-date'),
-		latestCouponDate = document.querySelectorAll('.latest-coupon-date');
+		couponPeriod = document.querySelectorAll('.coupon-period')[0],
+		maturityDate = document.querySelectorAll('.maturity-date'),
+		latestCouponDate = document.getElementById('latest-coupon-date');
 
+	// const newDate = new Date();
+	// console.log(newDate);
+	// newDate.setDate(newDate.getDate() + 50);
+	// document.getElementById('nearest-coupon-date').innerHTML = newDate;
 
+	let msec = Date.parse(latestCouponDate.innerHTML);
+	console.log(msec);
+	let newMsec = msec + ()
+	const newDate = new Date(msec);
+	console.log(newDate);
 })
